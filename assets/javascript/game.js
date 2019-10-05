ddf802a0bbe0904e84227df63658e2a20183c5ff
mike881205 <mike881205@gmail.com>
2019-10-05 09:03:22 -0400

    created if statement to automatically add spaces when needed in guess word

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,7 @@
 // ================================================================================================================================================
 // Global Variables
 // ================================================================================================================================================
+
 // Create an array of words
 var words = [
 "michael myers", 
@@ -16,26 +17,67 @@ var words = [
 "jack torrance"
 ];
 
+// var wordsPic = [];
+
+// wordsPic[0] = new Image();
+// wordsPic[0].src = "assets/images/michaelMyers.jpg";
+
+// wordsPic[1] = new Image();
+// wordsPic[1].src = 'images/img/Splash_image2.jpg';
+
+// wordsPic[2] = new Image();
+// wordsPic[2].src = 'images/img/Splash_image3.jpg';
+
+// wordsPic[3] = new Image();
+// wordsPic[3].src = 'images/img/Splash_image4.jpg';
+
+// wordsPic[4] = new Image();
+// wordsPic[4].src = 'images/img/Splash_image5.jpg';
+
+// wordsPic[5] = new Image();
+// wordsPic[5].src = 'images/img/Splash_image6.jpg';
+
 // Pick a random word
 var word = words[Math.floor(Math.random() * words.length)];
 console.log(word);
 
 //DOM Manipulation
-// DOM manipulation
 var docUnderScore = document.getElementsByClassName("underscore");
 var docWrongGuess = document.getElementsByClassName("wrongGuess");
-var docPrompt = document.getElementsByClassName("prompt");
-// Create answer array
+
+// Match Picture to word
+
+
+// var x = myFunction(words[i], wordsPic[i])
+
+// function myFunction(a, b) {
+//     if (a = words[0]) {
+//         return b = wordsPic[0]
+//     }   
+    
+// }
+// console.log(x)
+
+
+
+
+
+// Create answer arrays
 var answerArray = [];
 for ( var i = 0; i < word.length; i++) {
-    answerArray[i] = "_";
+    if (word[i] === " ") {
+        answerArray[i] = " ";
+    }else {
+    answerArray[i] = "_"    
+    }
 }
-
+docUnderScore[0].innerHTML = answerArray.join(" ");
 var wrongLetter = [];
 var remainingLetters = word.length;
 
-//Game Loop
-
+// ================================================================================================================================================
+// Game
+// ================================================================================================================================================
 
 //Get guess from player
 document.addEventListener("keypress", (event) => {
@@ -46,9 +88,10 @@ for (var j = 0; j < word.length; j++) {
         remainingLetters--;
     }
   };
-
+// If user's gues is correct
   if (answerArray.join("") === word) {
     alert("You Survived!");
+// If user's guess is incorrect
 } else {
     wrongLetter.push(guess);
     docWrongGuess[0].innerHTML = wrongLetter;
