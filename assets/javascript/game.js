c583a0e9f3705d6a7c09f705545408927a0dc2a4
mike881205 <mike881205@gmail.com>
2019-10-05 22:23:01 -0400

    Added CSS background and font (colors, family), adjusted heights and paddings of divs/elements, added hint image

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,98 +4,59 @@
 
 // Create an array of words
 var words = [
-"michael myers", 
-"freddy krueger", 
-"jason voorhees", 
-"xenomorph", 
-"pinhead", 
-"ghostface", 
-"hannibal lector", 
-"pennywise", 
-"leatherface", 
-"chucky", 
-"jack torrance"
-];
-
-// var wordsPic = [];
-
-// wordsPic[0] = new Image();
-// wordsPic[0].src = "assets/images/michaelMyers.jpg";
-
-// wordsPic[1] = new Image();
-// wordsPic[1].src = 'images/img/Splash_image2.jpg';
-
-// wordsPic[2] = new Image();
-// wordsPic[2].src = 'images/img/Splash_image3.jpg';
-
-// wordsPic[3] = new Image();
-// wordsPic[3].src = 'images/img/Splash_image4.jpg';
-
-// wordsPic[4] = new Image();
-// wordsPic[4].src = 'images/img/Splash_image5.jpg';
-
-// wordsPic[5] = new Image();
-// wordsPic[5].src = 'images/img/Splash_image6.jpg';
-
-// Pick a random word
-var word = words[Math.floor(Math.random() * words.length)];
-console.log(word);
-
-//DOM Manipulation
-var docUnderScore = document.getElementsByClassName("underscore");
-var docWrongGuess = document.getElementsByClassName("wrongGuess");
-
-// Match Picture to word
-
-
-// var x = myFunction(words[i], wordsPic[i])
-
-// function myFunction(a, b) {
-//     if (a = words[0]) {
-//         return b = wordsPic[0]
-//     }   
+    "michael myers", 
+    "freddy krueger", 
+    "jason voorhees", 
+    "pinhead", 
+    "ghostface", 
+    "pennywise", 
+    "leatherface", 
+    "chucky", 
+    ];
     
-// }
-// console.log(x)
+    
+    // Pick a random word
+    var word = words[Math.floor(Math.random() * words.length)];
+    console.log(word);
+    
+    //DOM Manipulation
+    var docUnderScore = document.getElementsByClassName("underscore");
+    var docWrongGuess = document.getElementsByClassName("wrongGuess"); 
 
-
-
-
-
-// Create answer arrays
-var answerArray = [];
-for ( var i = 0; i < word.length; i++) {
-    if (word[i] === " ") {
-        answerArray[i] = " ";
-    }else {
-    answerArray[i] = "_"    
+    // Create answer arrays
+    var answerArray = [];
+    for ( var i = 0; i < word.length; i++) {
+        if (word[i] === " ") {
+            answerArray[i] = " ";
+        }else {
+        answerArray[i] = "_"    
+        }
     }
-}
-docUnderScore[0].innerHTML = answerArray.join(" ");
-var wrongLetter = [];
-var remainingLetters = word.length;
-
-// ================================================================================================================================================
-// Game
-// ================================================================================================================================================
-
-//Get guess from player
-document.addEventListener("keypress", (event) => {
-var guess = event.key.toLowerCase();
-for (var j = 0; j < word.length; j++) {
-    if (word[j] === guess) {
-        answerArray[j] = guess;
-        remainingLetters--;
-    }
-  };
-// If user's gues is correct
-  if (answerArray.join("") === word) {
-    alert("You Survived!");
-// If user's guess is incorrect
-} else {
-    wrongLetter.push(guess);
-    docWrongGuess[0].innerHTML = wrongLetter;
-};
-
-docUnderScore[0].innerHTML = answerArray.join(" ");
-})
+    docUnderScore[0].innerHTML = answerArray.join("&nbsp");
+    var wrongLetter = [];
+    var remainingLetters = word.length;
+    
+    // ================================================================================================================================================
+    // Game
+    // ================================================================================================================================================
+    
+    //Get guess from player
+    document.addEventListener("keypress", (event) => {
+    var guess = event.key.toLowerCase();
+    for (var j = 0; j < word.length; j++) {
+        if (word[j] === guess) {
+            answerArray[j] = guess;
+            remainingLetters--;
+        }
+      };
+    // If user's gues is correct
+      if (answerArray.join("") === word) {
+        alert("You Survived!");
+    // If user's guess is incorrect
+    } else {
+        wrongLetter.push(guess.toUpperCase());
+        docWrongGuess[0].innerHTML = wrongLetter;
+    };
+    
+    docUnderScore[0].innerHTML = answerArray.join("&nbsp");
+    })
--- assets/javascript/game2.js
+++ assets/javascript/game2.js
@@ -1,98 +1,100 @@
-// // ================================================================================================================================================
-// // Global Variables
-// // ================================================================================================================================================
-// // Create an array of words
-// var word = [
-//     "michael myers", "freddy krueger", "jason voorhees", "xenomorph", "pinhead", "ghostface", "hannibal lector", "pennywise", "leatherface", "chucky", "jack torrance"]
-
-// var rightLetter = [];
-// var wrongLetter = [];
-// var underScore = [];
-
-// // Choose word randomly
-//     var randNum = Math.floor(Math.random() * word.length);
-//     var randWord = word[randNum];
-//     console.log(randWord);
-
-// // DOM manipulation
-// var docUnderScore = document.getElementsByClassName("underscore");
-// var docRightGuess = document.getElementsByClassName("rightGuess");
-// var docWrongGuess = document.getElementsByClassName("wrongGuess");
-
-
-// // ================================================================================================================================================
-// // Main
-// // ================================================================================================================================================
-// // Create underscore based on length of word
-//     var generateUnderscore = () => {
-//         for ( var i = 0; i < randWord.length; i++) {
-//             underScore.push("_");
-//         }
-//         return underScore;
-//     }
-
-// // Get user guess
-//     document.addEventListener("keypress", (event) => {
-//         var letterPressed = String.fromCharCode(event.keyCode);
-//         // if user's guess is correct    
-//         if (randWord.indexOf(letterPressed) > -1) {
-//         // replace underscore with correct letter
-//             underScore[randWord.indexOf(letterPressed)] = letterPressed;
-//             docUnderScore[0].innerHTML = underScore.join(" ");
-            
-//         // check to see if user word matches guess
-//             if (underScore.join("") === randWord) {
-//                 alert("You Survived!");
-//             }
-//         }
-//         // if user's guess is incorrect     
-//         else {
-//             wrongLetter.push(letterPressed);
-//             docWrongGuess[0].innerHTML = wrongLetter;
-//         }
-
-//     });
-
-//     docUnderScore[0].innerHTML = generateUnderscore().join(" ");
-
+// ================================================================================================================================================
+// Global Variables
+// ================================================================================================================================================
 
 // Create an array of words
-var words = [
-    "javascript",
-    "monkey",
-    "amazing",
-    "pancake"
-    ];
-    // Pick a random word
-    var word = words[Math.floor(Math.random() * words.length)];
-    // Set up the answer array
-    var answerArray = [];
-    for (var i = 0; i < word.length; i++) {
+let words = [
+    "michael myers", 
+    "freddy krueger", 
+    "jason voorhees", 
+    "xenomorph", 
+    "pinhead", 
+    "ghostface", 
+    "hannibal lector", 
+    "pennywise", 
+    "leatherface", 
+    "chucky", 
+    "jack torrance"
+];
+
+let guessQuote = [
+    "The Shape, Pure Evil",
+    "Man of your dreams, 'Welcome to prime time!'",
+    "His favorite sport is hockey, Had a bad experience at summer camp",
+    "The 'Perfect Organism', ",
+    "'You Opened the Box!', ",
+    "'What's your favorite movie?'",
+    "Enjoys a nice Chianti with his dinner",
+    "Sleeps for 27 years, A floater",
+    "From the Lone Star state, Fond of mechanical landscaping tools",
+    "One of the 'Good Guys', Doesn't play well with other children",
+    "A dull boy, 'Here's Johnny!'"
+]
+
+const maxTries = 10
+let remainingGuesses = 0;
+let gameStarted = false;
+let hasFinished = false;
+let wins = 0;
+let answerArray = [];
+let wrongLetter = [];
+let remainingLetters = word.length;
+
+// Reset game
+function resetGame() {
+    remainingGuesses = maxTries;
+    gameStarted = false;
+    answerArray = [];
+    wrongLetter = [];
+    for ( let i = 0; i < word.length; i++) {
+        answerArray[i] = "_";
+        if (word[i] === " ") {
+            answerArray[i] = " ";
+    }
+}};
+docUnderScore[0].innerHTML = answerArray.join("&nbsp");
+
+
+
+// Pick a random word
+let word = words[Math.floor(Math.random() * words.length)];
+console.log(word);
+
+//DOM Manipulation
+let docUnderScore = document.getElementsByClassName("underscore");
+let docWrongGuess = document.getElementsByClassName("wrongGuess");
+
+// Create answer arrays
+let answerArray = [];
+for ( let i = 0; i < word.length; i++) {
     answerArray[i] = "_";
-    }
-    var remainingLetters = word.length;
-    // The game loop
-    while (remainingLetters > 0) {
-    // Show the player their progress
-    alert(answerArray.join(" "));
-    // Get a guess from the player
-    var guess = prompt("Guess a letter, or click Cancel to stop playing.");
-    if (guess === null) {
-    // Exit the game loop
-    break;
-    } else if (guess.length !== 1) {
-    alert("Please enter a single letter.");
-    } else {
-    // Update the game state with the guess
-    for (var j = 0; j < word.length; j++) {
+    if (word[i] === " ") {
+        answerArray[i] = " ";
+}
+}
+docUnderScore[0].innerHTML = answerArray.join("&nbsp"); 
+
+// ================================================================================================================================================
+// Game
+// ================================================================================================================================================
+
+//Get guess from player
+document.addEventListener("keypress", (event) => {
+let guess = event.key.toLowerCase();
+for (let j = 0; j < word.length; j++) {
     if (word[j] === guess) {
-    answerArray[j] = guess;
-    remainingLetters--;
+        answerArray[j] = guess.toUpperCase();
+        remainingLetters--;
     }
-    }
-    }
-    // The end of the game loop
-    }
-    // Show the answer and congratulate the player
-    alert(answerArray.join(" "));
-    alert("Good job! The answer was " + word);
+  };
+// If user's gues is correct
+  if (answerArray.join("") == word) {
+    alert("You Survived!");
+// If user's guess is incorrect
+} else {
+    wrongLetter.push(guess.toUpperCase());
+    docWrongGuess[0].innerHTML = wrongLetter;
+};
+
+docUnderScore[0].innerHTML = answerArray.join("&nbsp");
+})
